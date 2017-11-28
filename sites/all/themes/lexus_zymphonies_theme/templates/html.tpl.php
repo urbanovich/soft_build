05e91eba6047f87e8ebca9d0e838cdf5d667396a
Ubuntu <ubuntu@soft-build.bsofy>
2017-11-28 10:54:55 +0000

    updated server files

--- sites/all/themes/lexus_zymphonies_theme/templates/html.tpl.php
+++ sites/all/themes/lexus_zymphonies_theme/templates/html.tpl.php
@@ -6,6 +6,16 @@
 <?php print $scripts; ?>
 <!--[if IE 8 ]>    <html class="ie8 ielt9"> <![endif]-->
 <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109024568-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-109024568-1');
+</script>
+
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print $page_top; ?>
